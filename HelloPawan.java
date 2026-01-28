227288f79be6c8e402f779207054578182b0f9c3
pawanpandey04 <pawanpandeywork4@gmail.com>
2026-01-28 21:41:39 +0530

    Add files via upload

---
+++ HelloPawan.java
@@ -0,0 +1,9 @@
+package basic_java;
+
+public class HelloPawan {
+    static void main(String[] args) {
+        System.out.println("hello pawan");
+        System.out.println("hello ritik");
+        System.out.println("hello ji");
+    }
+}
