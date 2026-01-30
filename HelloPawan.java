e0246914b51c5ca01d345e4e8c288a3bb2d49301
pawanpandey04 <pawanpandeywork4@gmail.com>
2026-01-30 22:23:57 +0530

    Delete HelloPawan.java

--- HelloPawan.java
+++
@@ -1,9 +0,0 @@
-package basic_java;
-
-public class HelloPawan {
-    static void main(String[] args) {
-        System.out.println("hello pawan");
-        System.out.println("hello ritik");
-        System.out.println("hello ji");
-    }
-}
