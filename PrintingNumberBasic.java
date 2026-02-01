58246b9097c5b25cf0dc9af49aa3a583e7b7fb78
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-01 13:09:46 +0530

    PrintingNumberBasic

---
+++ PrintingNumberBasic.java
@@ -0,0 +1,28 @@
+package basic_java;
+
+//public class PrintingNumberBasic {
+//    static void main(String[] args) {
+//        System.out.println(69);
+//
+//    }
+//}
+
+
+              //  what difference between uper and niche
+
+//public class PrintingNumberBasic {
+//    static void main(String[] args) {
+//        System.out.println("69");
+//
+//    }
+//}
+
+
+
+public class PrintingNumberBasic {
+    static void main(String[] args) {
+        System.out.println("69+1");         // print those who will given by it
+        System.out.println(69+1);           // it is a only number those it print automatic
+
+    }
+}
