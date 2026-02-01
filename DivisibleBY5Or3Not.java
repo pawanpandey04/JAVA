57486b70844ad4fa4cfcab44b733e3b684c709c8
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-01 13:46:14 +0530

    Divisibleby5Or3Not

---
+++ DivisibleBY5Or3Not.java
@@ -0,0 +1,17 @@
+package if_else;
+
+// Take a positive intege input and tell if it is divisible by 5 or 3.
+
+import java.util.Scanner;
+
+public class DivisibleBY5Or3Not {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = sc.nextInt();
+        if(n%3==0 || n%5==0){
+            System.out.println("it is divisible by 5 or 3");
+        }
+        else System.out.println("it is not divisible by 5 or 3");
+    }
+}
