966257ec8bf54b131f5834f2c67f78f7877d51e3
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-01 12:55:59 +0530

    CharDataType

---
+++ CharDataType.java
@@ -0,0 +1,12 @@
+public class CharDataType {
+    static void main(String[] args) {
+        char ch = 'A';
+        int x = ch;
+        System.out.println(x);
+        System.out.println(ch);
+
+        char pawan = 'a';
+        int y = (int)pawan;  // explicit typecasting
+        System.out.println(y);
+    }
+}
