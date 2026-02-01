ef6b8bd651a76be9060220d6b4cf56ccd832c640
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-01 14:05:27 +0530

    QuadrantOfPoint

---
+++ QuadrantOfPoint.java
@@ -0,0 +1,17 @@
+package if_else;
+
+//          Hw
+// Given a point (x,y), write a program to find out if it is lies in the 1st qudrant, 2nd qudrant
+// 3rd qudrant, 4th qudrant, on the x-axis, y-axis or at the orgin
+
+import java.util.Scanner;
+
+public class QuadrantOfPoint {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enteer number");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+    }
+}
