c65f236c78ff5afaa2d9d7018cb5f7e5e5b0339d
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-01 14:23:02 +0530

    TernaryBasic

---
+++ TernaryBasic.java
@@ -0,0 +1,16 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class TernaryBasic {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number ");
+        int n = sc.nextInt();
+//        if(n%2 == 0) System.out.println("even number ");
+//        else System.out.println("odd number");
+
+            // condition ? sach : jhooth
+        System.out.println((n%2 == 0) ? "even" : "odd");
+    }
+}
