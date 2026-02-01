9a79322ed39776a2b7b4f88bcadffa91ba576cb4
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-01 13:14:57 +0530

    VariableBasic

---
+++ VariableBasic.java
@@ -0,0 +1,48 @@
+package basic_java;
+//
+//public class VariableBasic {
+//    static void main(String[] args) {
+//        int x =5;
+//        int y =8;
+//        int z =12;
+//        System.out.print(z);
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z+254);
+//        System.out.println(x+56);
+//        System.out.println(y+86);
+//        System.out.println(z-x);
+//        System.out.println( y-x);
+//        System.out.println(  z*x);
+//        System.out.println(  x*y);
+//
+//    }
+//}
+
+
+import java.sql.SQLOutput;
+
+//public class VariableBasic {
+//    static void main(String[] args) {
+//        int x = 5;
+//        System.out.println(x);
+//        x = 9;
+//        System.out.println(x);
+//        System.out.println(x*5);
+//    }
+//}
+
+public class VariableBasic {
+    static void main(String[] args){
+        int x = 9;
+        System.out.println(x);
+        x = x-3;
+        System.out.println(x);
+        x = x+5;
+        System.out.println(x);
+        x = x*10;
+        System.out.println(x);
+        x = x/11;
+        System.out.println(x);
+    }
+}
