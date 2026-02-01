1ee0ca2fa81771089b6c353e2a6035883a5f2fb5
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-01 13:01:49 +0530

    DivisionOfDataType

---
+++ DivisionOfDatatype.java
@@ -0,0 +1,19 @@
+package basic_java;
+
+//public class DivisionOfDatatype {
+//    static void main(String[] args) {
+//        double x = 5/2;                //  output 2
+//        System.out.println(x);         //   integer by integer === integer
+//    }
+//}
+
+
+            //  and same code
+
+
+public class DivisionOfDatatype {
+    static void main(String[] args) {
+        double x = 5.0 / 2;                //  output 2.5
+        System.out.println(x);            //   decimal by integer === decimal  (one are decimal then give me a decimal number)
+    }
+}
