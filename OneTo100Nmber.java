39fc9a6ea2c3f51468794c1073799ffb486f3239
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-04 21:19:07 +0530

    OneTo100Number

---
+++ OneTo100Nmber.java
@@ -0,0 +1,25 @@
+package loops;
+
+// Print 1 to 100 numbers
+
+import java.util.Scanner;
+
+// public class OneTo100Nmber {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter number : ");
+//         int n = sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+public class OneTo100Nmber{
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
