346194524e404bf7c8481856b1b4f7a33a977b20
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-04 21:12:05 +0530

    LimitOfInt

---
+++ LimitOfInt.java
@@ -0,0 +1,30 @@
+package loops;
+  
+                     // FIND LONG INTEGER NUMBER
+
+// public class LimitOfInt {
+// //    public static void main(String[] args) {
+// //     int x = Integer.MAX_VALUE;
+// //     System.out.println(x);
+// //    }
+    
+// }
+
+
+//                   LONG ADD NUMBER INTEGER
+
+// public class LimitOfInt {
+//     public static void main(String[] args) {
+//         long x = (long) 2147483647 + 10;
+//         System.out.println(x);
+//     }
+// }
+
+            //LONG VALUE
+
+public class LimitOfInt {
+    public static void main(String[] args) {
+        long x = Long.MAX_VALUE;
+        System.out.println(x);
+    }
+}     
