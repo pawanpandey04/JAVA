a656bf1b7b3a42c4d0a209f0bf444a7f653c6b6d
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-04 21:01:29 +0530

    BasicLoops

---
+++ BasicLoops.java
@@ -0,0 +1,30 @@
+package loops;
+//
+//import java.util.Scanner;
+//
+//public class BasicLoops {
+//    static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number: ");
+//        int n = sc.nextInt();
+//        for(int i=1; i<=n; i++){
+////            System.out.println("pawan");   // many time to print pawan
+////            System.out.print(i+" ");    // print count number
+//        }
+//    }
+//}
+
+
+
+import java.util.Scanner;
+
+public class BasicLoops {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        for( i=1; i<=8; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println(i);
+    }
+}
