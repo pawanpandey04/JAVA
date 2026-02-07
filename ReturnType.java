c86b712c4ef86b6c72c7bd3c6e63473564f6fca5
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-08 01:09:41 +0530

    ReturnType

---
+++ ReturnType.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class ReturnType {
+    public static int prasant(){
+        System.out.println("bhanu");
+        System.out.println("Nanda");
+        return 7;
+    }
+    public static void main(String[] args) {
+        System.out.println(9+prasant());
+    }
+    
+}
