fd15135727ea85ac1836f2625541e19e7909add2
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-08 01:06:24 +0530

    PassByValueAndRefrence

---
+++ PassByValueAndReference.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class PassByValueAndReference {
+    public static void change(int x){
+        x=10;
+    }
+    public static void main(String[] args) {
+        int x =6;
+        System.out.println(x);
+        change(8);
+        System.out.println(x);
+    }
+    
+}
