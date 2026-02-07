06cf65f1f1c8b0b664fcf65f94d9c489e76d7952
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-08 01:04:44 +0530

    Nuitln

---
+++ NuiltIn.java
@@ -0,0 +1,21 @@
+package Methods;
+
+public class NuiltIn {
+    public static void main(String[] args) {
+        // System.out.println(Math.sqrt(4));
+        // double x = Math.sqrt(4);
+        System.out.println(Math.sqrt(8)); // square number
+        System.out.println(Math.cbrt(99));  // cube number
+        System.out.println(Math.cbrt(125));  // cube number
+                System.out.println(Math.sqrt(125)); // square
+                System.out.println(Math.abs(7654));  //absolute value
+                 System.out.println(Math.abs(-7654));   //absolute value
+                 System.out.println(Math.floor(6.9));  // is number se chota number kya hoga
+                  System.out.println(Math.floor(-6.9));   // is number se chota number kya hoga
+                  System.out.println(Math.ceil(3.987));  // ceil value is 4 q ki ye 4 integer m exist krta h 
+                  System.out.println(Math.min(3,1.8));  // minima
+                  System.out.println(Math.min(1.3,9.8));  // minima in number
+
+    }
+    
+}
