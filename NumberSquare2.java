3379e302cbf767c1bc979e4b156c13912249c998
pawan pandey <pawanpandeywork4@gmail.com>
2026-02-08 11:37:34 +0530

    NumberSquare2

---
+++ NumberSquare2.java
@@ -0,0 +1,30 @@
+package Pattern_printing;
+
+// import java.util.Scanner;
+
+// public class NumberSquare2 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int row = sc.nextInt();
+//         int col = sc.nextInt();
+//         for(int i=1;i<=row;i++){
+//             for(int j=1;j<=col;j++){
+//                 System.out.print(i+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+public class NumberSquare2{
+    public static void main(String[] args){
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
